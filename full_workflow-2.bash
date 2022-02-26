4ca7cf53effb3d1cc4d2ae964678024b8e47a129
KyuoFoxHuyu <88403766@qq.com>
2022-02-27 01:56:39 +0800

    TMP
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- full_workflow-2.bash
+++ full_workflow-2.bash
@@ -10,18 +10,23 @@ cd ${TOPLEV}
 echo "Cloning LLVM"
 
 git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git pull
+cd ..
 
 echo "Building Stage 1 Compiler"
 
 mkdir ${TOPLEV}/stage1
 cd ${TOPLEV}/stage1
 
-cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_ASM_COMPILER=gcc \
       -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt;bolt" \
       -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_XRAY=OFF \
       -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+      -DCLANG_VENDOR="Kyuofox-$(date +%Y%m%d)" \
+	  -DCLANG_REPOSITORY_STRING="GitHub.com/KyuoFoxHuyu" \
       -DCMAKE_INSTALL_PREFIX=${TOPLEV}/stage1/install
       
 ninja install
@@ -32,12 +37,14 @@ mkdir ${TOPLEV}/stage2-prof-gen
 cd ${TOPLEV}/stage2-prof-gen
 CPATH=${TOPLEV}/stage1/install/bin/
 
-cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
     -DLLVM_ENABLE_PROJECTS="clang;lld" \
 	-DLLVM_PARALLEL_LINK_JOBS="$(jobs)" \
     -DLLVM_USE_LINKER=lld -DLLVM_BUILD_INSTRUMENTED=ON \
+    -DCLANG_VENDOR="Kyuofox-$(date +%Y%m%d)" \
+	-DCLANG_REPOSITORY_STRING="GitHub.com/KyuoFoxHuyu" \
     -DCMAKE_INSTALL_PREFIX=${TOPLEV}/stage2-prof-gen/install
 ninja install
 
@@ -48,11 +55,13 @@ mkdir ${TOPLEV}/stage3-train
 cd ${TOPLEV}/stage3-train
 CPATH=${TOPLEV}/stage2-prof-gen/install/bin
 
-cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
     -DLLVM_ENABLE_PROJECTS="clang" \
 	-DLLVM_PARALLEL_LINK_JOBS="$(jobs)" \
+	-DCLANG_VENDOR="Kyuofox-$(date +%Y%m%d)" \
+	-DCLANG_REPOSITORY_STRING="GitHub.com/KyuoFoxHuyu" \
     -DLLVM_USE_LINKER=lld -DCMAKE_INSTALL_PREFIX=${TOPLEV}/stage3-train/install
 ninja clang
 
@@ -70,11 +79,13 @@ CPATH=${TOPLEV}/stage1/install/bin/
 
 export LDFLAGS="-Wl,-q"
 
-cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
     -DLLVM_ENABLE_PROJECTS="clang;lld" \
     -DLLVM_ENABLE_LTO=Thin \
+    -DCLANG_VENDOR="Kyuofox-$(date +%Y%m%d)" \
+	-DCLANG_REPOSITORY_STRING="GitHub.com/KyuoFoxHuyu" \
 	-DLLVM_PARALLEL_LINK_JOBS="$(jobs)" \
     -DLLVM_PROFDATA_FILE=${TOPLEV}/stage2-prof-gen/profiles/clang.profdata \
     -DLLVM_USE_LINKER=lld \
@@ -88,11 +99,15 @@ mkdir ${TOPLEV}/stage3
 cd ${TOPLEV}/stage3
 CPATH=${TOPLEV}/stage2-prof-use-lto/install/bin/
 
-cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release \
+cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
+    -DCLANG_VENDOR="Kyuofox-$(date +%Y%m%d)" \
+	-DCLANG_REPOSITORY_STRING="GitHub.com/KyuoFoxHuyu" \
+	-DLLVM_ENABLE_PROJECTS="clang;lld;polly;bolt" \
+	-DLLVM_ENABLE_LTO=Thin \
     -DLLVM_USE_LINKER=lld -DCMAKE_INSTALL_PREFIX=${TOPLEV}/stage3/install
 
-perf record -e cycles:u -j any,u -- ninja
+perf record -e cycles:u ,u -- ninja
 
 echo "Converting profile to a more aggreated form suitable to be consumed by BOLT"
 
